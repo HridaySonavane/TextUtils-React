7fa14d98f3a964777d046ed4a56d343489e3c0d8
HridaySonavane <hridays2005@gmail.com>
2024-01-19 20:33:43 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import About from "./components/About";
+// import About from "./components/About";
 import Alerts from "./components/Alerts";
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light"); //state whether dark mode is enabled or not
@@ -34,22 +34,24 @@ function App() {
   };
 
   return (
-    <Router>
+    // <Router>
+    <>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alerts alert={alert} />
       <div className="container">
-        <Routes>
-          <Route
-            exact
+        {/* <Routes> */}
+          {/* <Route */}
+            {/* exact
             path="/"
-            element={
+            element={ */}
               <TextForm heading="Enter the text to analyse below" mode={mode} />
-            }
-          />
-          <Route exact path="about/*" element={<About mode={mode} />} />
-        </Routes>
+            {/* } */}
+          {/* /> */}
+          {/* <Route exact path="about/*" element={<About mode={mode} />} /> */}
+        {/* </Routes> */}
       </div>
-    </Router>
+      {/* </Router> */}
+    </>
   );
 }
 
