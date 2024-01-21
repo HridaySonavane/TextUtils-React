b46b88d9caf6e351d9b6efe2053feb78588bc039
HridaySonavane <hridays2005@gmail.com>
2024-01-21 23:29:06 +0530

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ function About(props) {
     <div className={`container py-1 text-${props.mode === 'light'? 'dark':'light'}`}>
       <h1 className="m-3">About Us</h1>
 
-      <div className={`accordion`} id="accordionExample" data-bs-theme={`${props.mode === 'light'? 'light':'dark'}`} >
+      <div className={`accordion`} id="accordionExample" data-bs-theme={`${props.mode}`} >
         <div className="accordion-item">
           <h2 className="accordion-header">
             <button
